20db715b33eeca796a8fcbfb31d034f2a17eb797
salmandhedhi7 <m.shahid.dhedhi@gmail.com>
2023-08-29 14:31:23 +0500

    yyyyy

---
+++ message/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+import { Stack, Button } from "react-bootstrap";
+import { ArrowRight, Envelope, HandThumbsUp, ChatLeftText, Share } from "react-bootstrap-icons";
+import profileImg from "./img/profile.jpeg";
